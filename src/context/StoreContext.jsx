f7c9c4ca3447769509b2567da7b99ae76584248c
Akash kushwaha <kushwahavijay1893@gmail.com>
2025-10-11 22:44:52 +0530

    Update StoreContext.jsx

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ const StoreContextProvider = (props) => {
   const [food_list, setFoodList] = useState([]);
   const [token, setToken] = useState("");
 
-  const url = "https://backendeatagain-3q1a.vercel.app";
+  const url = "https://food-del-backend-bter.onrender.com";
 
   // Fetch food list
   const fetchFoodList = async () => {
@@ -120,6 +120,7 @@ const StoreContextProvider = (props) => {
 };
 
 export default StoreContextProvider;
+
 
 
 
